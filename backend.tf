7a0e018d2d48372a1232801b59b39e7c11c2ae3c
Simon Brosdetzko <simon.brosdetzko@iu-study.org>
2026-03-19 01:56:24 +0100

    update

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket       = "sbiu-terraform-state"
-    key          = "terraform.tfstate"
-    region       = "eu-central-1"
-    encrypt      = true
-    use_lockfile = true
-  }
+terraform { 
+	backend "s3" {  
+		bucket = "sbiu-terraform-state" 
+		key = "terraform.tfstate"
+		region = "eu-central-1"
+		encrypt = true
+        use_lockfile = true
+        
+	}
 }
