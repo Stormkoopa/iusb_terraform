5d094e0d17b2116d66136a26539e8e86dd66da06
Simon Brosdetzko <simon.brosdetzko@iu-study.org>
2026-03-19 01:55:45 +0100

    format

--- variables.tf
+++ variables.tf
@@ -7,6 +7,6 @@ variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
 variable "private_data_subnet_az2_cidr" {}
-variable "zones"{}
-variable "public_subnets"{}
-variable "private_subnets"{}
+variable "zones" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
